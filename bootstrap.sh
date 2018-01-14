69f27726ad3aeb1e7f971776be8fc576dfa9f3f4
nerdmarshall <marshall@sprux.com>
2018-01-14 07:03:07 -0800

    tmp change of hardcoded repo url
    (will revert)

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,10 @@ if [ -d ~/.electrumx-installer ]; then
     exit 1
 fi
 if which git > /dev/null 2>&1; then
-    git clone https://github.com/bauerj/electrumx-installer ~/.electrumx-installer
+    git clone https://github.com/sprux/electrumx-installer ~/.electrumx-installer
 else
     which wget > /dev/null 2>&1 && which unzip > /dev/null 2>&1 || { echo "Please install git or wget and unzip" && exit 1 ; }
-    wget https://github.com/bauerj/electrumx-installer/archive/master.zip -O /tmp/electrumx-master.zip
+    wget https://github.com/sprux/electrumx-installer/archive/master.zip -O /tmp/electrumx-master.zip
     unzip /tmp/electrumx-master.zip -d ~/.electrumx-installer
     rm /tmp/electrumx-master.zip
 fi
